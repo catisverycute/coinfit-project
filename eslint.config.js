79cedf30453122af93625c766415ad55c4c03969
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-12 18:27:09 +0900

    프로젝트 초기 세팅

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'eslint/config';
+import js from '@eslint/js';
+import globals from 'globals';
+import tseslint from 'typescript-eslint';
+import pluginReact from 'eslint-plugin-react';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    plugins: { js, prettier: require('eslint-plugin-prettier') },
+    extends: ['js/recommended'],
+    rules: {
+      'prettier/prettier': 'error',
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,ts,jsx,tsx}'],
+    languageOptions: { globals: { ...globals.browser, ...globals.node } },
+  },
+  tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+]);
