774a755a0e83f6fa9af8efc4fd3fc34c0e8583c2
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-19 23:27:36 +0900

    chore: 경로 수정

--- src/pages/MainPage.tsx
+++ src/pages/MainPage.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import SpendingCard from '../components/Home/SpendingCard';
-import RecentPayCard from '../components/Home/RecentPayCard';
-import MonthlyGraphCard from '../components/Home/MonthlyGraphCard';
+import SpendingCard from '../components/home/SpendingCard';
+import RecentPayCard from '../components/home/RecentPayCard';
+import MonthlyGraphCard from '../components/home/MonthlyGraphCard';
 
 const MainPage: React.FC = () => {
   return (
