bb29096d46d8f7e14052a845d4843437a482b3d1
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-19 22:58:43 +0900

    chore: route 수정

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import LoginPage from './pages/LoginPage';
-import SignupPage from './pages/SignupPage';
-import MainPage from './pages/MainPage';
-import BottomNav from './components/BottomNav';
-import AccountListPage from './pages/AccountListPage';
+import { BrowserRouter } from 'react-router-dom';
+import Router from './router/Router';
 
 function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/account" element={<AccountListPage />} />
-      </Routes>
-      <BottomNav />
+      <Router />
     </BrowserRouter>
   );
 }
