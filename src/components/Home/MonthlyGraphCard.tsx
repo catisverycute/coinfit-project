e2e8bbb811d7d1eecae47cc6c101f30ec2485b0c
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-19 23:29:59 +0900

    chore: 소비 목표 그래프 컴포넌트 추가

--- src/components/Home/MonthlyGraphCard.tsx
+++ src/components/Home/MonthlyGraphCard.tsx
@@ -1,18 +1,10 @@
 import React from 'react';
-import dollar from '../../assets/icons/dollarIcon.svg';
+import SpendingProgress from '../analysis/SpendingProgress';
 
 const MonthlyGraphCard: React.FC = () => {
   return (
     <div className="border m-5 p-4 rounded-lg">
-      <div className="flex h-10">
-        <img src={dollar} alt="dollar" />
-        <div>이번 달 지출</div>
-      </div>
-      <div>30만원 / 70만원</div>
-      <div>
-        <progress id="progress" max="100" value="70" />
-        <label htmlFor="progress">70%</label>
-      </div>
+      <SpendingProgress current={500000} goal={700000} />
     </div>
   );
 };
