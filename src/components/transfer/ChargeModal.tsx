2c07b3abcfc4e518c0e2560044dbf71be9ca883e
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-19 23:25:53 +0900

    feat: 금액 충전 모달 생성

---
+++ src/components/transfer/ChargeModal.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import closeLg from '../../assets/icons/close-lg.svg';
+import Button from '../common/Button';
+
+interface ModalProps {
+  onClose: () => void;
+}
+
+const ChargeModal: React.FC<ModalProps> = ({ onClose }) => {
+  return (
+    <div className="border rounded-t-lg bg-white w-full fixed bottom-0 z-50">
+      <div className="pt-10 pb-10 pr-4 pl-4">
+        <div className="flex justify-between *:mb-6 text-3xl">
+          얼마나 충전할까요?
+          <Button className="bg-white border-none" onClick={onClose}>
+            <img className="w-8" src={closeLg} />
+          </Button>
+        </div>
+        <div className="border rounded-xl m-2 p-5 text-xl">가나 1234에서</div>
+        <div className="flex flex-col items-start ml-2 my-8 text-lg">
+          <button className="p-2">100만원</button>
+          <button className="p-2">50만원</button>
+          <button className="p-2">30만원</button>
+          <button className="p-2">20만원</button>
+          <button className="p-2">10만원</button>
+          <button className="p-2">직접 입력</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ChargeModal;
