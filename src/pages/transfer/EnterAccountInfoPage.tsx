92af4d671248369bd9553420367b970f1549bbe0
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-21 23:01:32 +0900

    feat: navigate 및 css 수정

--- src/pages/transfer/EnterAccountInfoPage.tsx
+++ src/pages/transfer/EnterAccountInfoPage.tsx
@@ -1,13 +1,21 @@
 import { useState } from 'react';
 import BankModal from '../../components/transfer/BankModal';
+import { useNavigate } from 'react-router-dom';
+import BackButton from '../../components/common/BackButton';
 
 const EnterAccountInfoPage = () => {
   const [accountNumber, setAccountNumber] = useState('');
   const [show, setShow] = useState<boolean>(false);
+  const navigate = useNavigate();
+
+  const nextStep = () => {
+    navigate('/transfer/step3');
+  };
 
   return (
-    <div>
-      <h2 className="text-xl font-bold mb-4">계좌번호 입력</h2>
+    <div className="p-6 mt-5">
+      <BackButton />
+      <h2 className="text-2xl font-bold mb-8">계좌번호 입력</h2>
       <div className="border rounded">
         <input
           type="tel"
@@ -16,15 +24,13 @@ const EnterAccountInfoPage = () => {
           value={accountNumber}
           onChange={(e) => setAccountNumber(e.target.value)}
           placeholder="계좌번호를 입력하세요"
-          className="border-b px-3 py-2 w-full"
+          className="border-b px-3 py-5 w-full"
         />
-        <button className="px-3 py-2" onClick={() => setShow(true)}>
+        <button className="px-3 py-5" onClick={() => setShow(true)}>
           은행 선택
         </button>
-        <select>
-          <option></option>
-        </select>
       </div>
+      <button onClick={nextStep}>확인</button>
       {show && <BankModal onClose={() => setShow(false)} />}
     </div>
   );
