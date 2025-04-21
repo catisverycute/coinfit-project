9f0fad78b1a25f8e213e2fad089707539be18a1b
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-21 22:59:02 +0900

    chore: BackButton 컴포넌트 추가

--- src/pages/transfer/SelectAccountPage.tsx
+++ src/pages/transfer/SelectAccountPage.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-// import Button from '../../components/common/Button';
+import BackButton from '../../components/common/BackButton';
 
 const SelectAccountPage: React.FC = () => {
   const navigate = useNavigate();
@@ -11,11 +11,11 @@ const SelectAccountPage: React.FC = () => {
 
   return (
     <div className="m-3">
+      <BackButton />
       <div className="text-3xl my-8">어떤 계좌로 보낼까요?</div>
       <button className="text-2xl mb-5" onClick={nextStep}>
         계좌번호 입력
       </button>
-      {/* <Button className="bg-white border-none">계좌번호 입력</Button> */}
       <div className="text-2xl">
         최근에 보낸 계좌
         <div className="border mt-5 p-3 rounded-lg">계좌1</div>
