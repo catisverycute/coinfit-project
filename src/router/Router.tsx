632e44d0e3a60fc755f0086acdfd48b47de7865d
catisverycute <105216377+catisverycute@users.noreply.github.com>
2025-04-27 23:04:30 +0900

    chore: header 조건으로 보이게 추가

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -1,10 +1,9 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
 import MainPage from '../pages/MainPage';
 import LoginPage from '../pages/auth/LoginPage';
 import SignupPage from '../pages/auth/SignupPage';
 import AccountListPage from '../pages/account/AccountListPage';
 import BottomNav from '../components/common/BottomNav';
-import { useLocation } from 'react-router-dom';
 import SelectAccountPage from '../pages/transfer/SelectAccountPage';
 import EnterAccountInfoPage from '../pages/transfer/EnterAccountInfoPage';
 import AnalysisPage from '../pages/analysis/AnalysisPage';
@@ -18,21 +17,43 @@ import BudgetSettingPage from '../pages/setting/BudgetSettingPage';
 import ExpenseButton from '../pages/analysis/ExpenseButton';
 import Header from '../components/common/Header';
 import ChargePage from '../pages/transfer/ChargePage';
+import PayAmountEnterPage from '../pages/pay/PayAmountEnterPage';
+import SelectPayAccountPage from '../pages/pay/SelectPayAccountPage';
+import SetPinPage from '../pages/auth/SetPinPage';
 
 const Router = () => {
   const location = useLocation();
+
+  const hideHeaderRoutes = [
+    '/login',
+    '/signup',
+    '/reset-password',
+    '/account',
+    '/menu',
+    '/account/transfer/step1',
+    '/account/transfer/step2',
+    '/account/transfer/step3',
+    '/account/transfer/step4',
+    '/account/transfer/success',
+  ];
+  const shouldHideHeader = hideHeaderRoutes.includes(location.pathname);
+
   const hideBottomNavRoutes = [
     '/login',
     '/signup',
     '/reset-password',
-    '/transfer/step1',
+    '/account/transfer/step1',
+    '/account/transfer/step2',
+    '/account/transfer/step3',
+    '/account/transfer/step4',
+    '/account/transfer/success',
     '/expense',
   ];
   const shouldHideBottomNav = hideBottomNavRoutes.includes(location.pathname);
 
   return (
     <>
-      <Header />
+      {!shouldHideHeader && <Header />}
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/reset-password" element={<ResetPasswordPage />} />
@@ -62,7 +83,7 @@ const Router = () => {
           }
         />
         <Route
-          path="account/transfer/step1"
+          path="/account/transfer/step1"
           element={
             <PrivateRoute>
               <SelectAccountPage />
@@ -70,7 +91,7 @@ const Router = () => {
           }
         />
         <Route
-          path="account/transfer/step2"
+          path="/account/transfer/step2"
           element={
             <PrivateRoute>
               <EnterAccountInfoPage />
@@ -78,7 +99,7 @@ const Router = () => {
           }
         />
         <Route
-          path="account/transfer/step3"
+          path="/account/transfer/step3"
           element={
             <PrivateRoute>
               <AmountEnterPage />
@@ -86,7 +107,7 @@ const Router = () => {
           }
         />
         <Route
-          path="account/transfer/step4"
+          path="/account/transfer/step4"
           element={
             <PrivateRoute>
               <TransferConfirmPage />
@@ -94,10 +115,18 @@ const Router = () => {
           }
         />
         <Route
-          path="account/transfer/success"
+          path="/account/transfer/success"
           element={
             <PrivateRoute>
               <RemitSuccessPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/set-pin"
+          element={
+            <PrivateRoute>
+              <SetPinPage />
             </PrivateRoute>
           }
         />
@@ -110,10 +139,26 @@ const Router = () => {
           }
         />
         <Route
-          path="account/charge"
+          path="/account/charge"
           element={
             <PrivateRoute>
               <ChargePage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/pay/transfer/step1"
+          element={
+            <PrivateRoute>
+              <SelectPayAccountPage />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/pay/transfer/step2"
+          element={
+            <PrivateRoute>
+              <PayAmountEnterPage />
             </PrivateRoute>
           }
         />
